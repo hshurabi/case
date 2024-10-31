98fbfa46098359e1f964d1d6e7e476749500dc38
Hamed <32173511+hshurabi@users.noreply.github.com>
2024-10-30 21:52:35 -0400

    Update __init__.py
    Changed metric names: i_ausc, m_ausc

--- __init__.py
+++ __init__.py
@@ -4,12 +4,12 @@ from .case import CASE
 # Import auxiliary modules from utils for convenience
 from .utils.model_selection import TSKFold
 from .utils.calibration import ABBQ
-from .utils.metrics import iAUSC, mAUSC
+from .utils.metrics import i_ausc, m_ausc
 
 __all__ = [
     "CASE",
     "TSKFold",
-    "ABBQ",
-    "iAUSC",
-    "mAUSC"
+    "i_ausc",
+    "m_ausc",
+    "ABBQ"
 ]
