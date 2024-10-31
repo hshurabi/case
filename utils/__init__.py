c6a178ae76df2893650a1a84b5e52baa26123d93
Hamed <32173511+hshurabi@users.noreply.github.com>
2024-10-30 21:49:54 -0400

    Updated __init__.py
    changed i_ausc and m_ausc names

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,11 +1,11 @@
 # Import functions or classes from each utility module
-from .calibration import ABBQ  
-from .metrics import iAUSC, mAUSC 
+from .calibration import ABBQ
+from .metrics import i_ausc,m_ausc 
 from .model_selection import TSKFold  
 
 __all__ = [
     "ABBQ",
-    "iAUSC",
-    "mAUSC",
+    "i_ausc",
+    "m_ausc",
     "TSKFold"
 ]
